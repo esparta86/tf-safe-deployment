a2e2af05c018bfbfc458fa99a3fdff661a75a2c9
lisandro_rafaelano <lisandro_rafaelano@epam.com>
2022-03-13 10:54:07 -0600

    Hello web app updated and Dockerfile

--- src-deployment/helloworld16/src/test/java/com/eks/Helloworld16ApplicationTests.java
+++ src-deployment/helloworld16/src/test/java/com/eks/Helloworld16ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloWorld17ApplicationTests {
+class Helloworld16ApplicationTests {
 
 	@Test
 	void contextLoads() {
