0e10492aab6e34bf8eb8c5cd53254dde60e94187
lisandro_rafaelano <lisandro_rafaelano@epam.com>
2022-03-14 06:15:39 -0600

    Readme updated and cleaned code

--- infra/nat.tf
+++ infra/nat.tf
@@ -8,23 +8,6 @@ resource "aws_eip" "nat" {
 }
 
 
-
-# resource "aws_nat_gateway" "nat" {
-#   allocation_id = aws_eip.nat.id
-#   subnet_id     = aws_subnet.eks-public-us-east-1a.id
-
-#   tags = merge(var.default_tags, {
-#     Name = "nat-eks-public-us-east-1a"
-#   })
-
-#   depends_on = [
-#     aws_internet_gateway.igw
-#   ]
-# }
-
-
-
-
 #  I thing it's enoutgh create one NAT for all
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
